2784a964ece3d0216672482992f5eb9d8b24ee3b
Rafael Franco <rafael.apfsantos@gmail.com>
2018-06-18 17:35:31 -0300

    master - test

--- module/Album/test/Controller/AlbumControllerTest.php
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -4,10 +4,16 @@ namespace AlbumTest\Controller;
 use Album\Controller\AlbumController;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Album\Model\AlbumTable;
+use Zend\ServiceManager\ServiceManager;
+use Album\Model\Album;
+use Prophecy\Argument;
+
 
 class AlbumControllerTest extends AbstractHttpControllerTestCase
 {
     protected $traceError = false;
+    protected $albumTable;
 
     public function setUp()
     {
@@ -23,15 +29,57 @@ class AlbumControllerTest extends AbstractHttpControllerTestCase
             $configOverrides
         ));
         parent::setUp();
+
+        $this->configureServiceManager($this->getApplicationServiceLocator());
     }
 
     public function testIndexActionCanBeAccessed()
     {
+        $this->albumTable->fetchAll()->willReturn([]);
+
         $this->dispatch('/album');
         $this->assertResponseStatusCode(200);
         $this->assertModuleName('Album');
         $this->assertControllerName(AlbumController::class);
         $this->assertControllerClass('AlbumController');
         $this->assertMatchedRouteName('album');
+    }
+
+    protected function configureServiceManager(ServiceManager $services)
+    {
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(AlbumTable::class, $this->mockAlbumTable()->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    protected function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
+
+    protected function mockAlbumTable()
+    {
+        $this->albumTable = $this->prophesize(AlbumTable::class);
+        return $this->albumTable;
+    }
+
+    public function testAddActionRedirectsAfterValidPost()
+    {
+        $this->albumTable
+            ->saveAlbum(Argument::type(Album::class))
+            ->shouldBeCalled();
+
+        $postData = [
+            'title'  => 'Led Zeppelin III',
+            'artist' => 'Led Zeppelin',
+            'id'     => '',
+        ];
+        $this->dispatch('/album/add', 'POST', $postData);
+        $this->assertResponseStatusCode(302);
+        $this->assertRedirectTo('/album');
     }
 }
